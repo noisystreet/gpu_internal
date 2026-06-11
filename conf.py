1b223a713690709f8ab05cbba8f87b79de280169
caecraft <caecraft@163.com>
2026-06-12 07:28:34 +0800

    修复侧边栏导航深度: navigation_depth 2→3 对齐 :maxdepth: 3

--- conf.py
+++ conf.py
@@ -24,7 +24,7 @@ language = "zh_CN"
 # -- Options for HTML output -------------------------------------------------
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    "navigation_depth": 2,
+    "navigation_depth": 3,
 }
 html_static_path = ["_static"]
 
